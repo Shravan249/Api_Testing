45ccf84c99da5d6668c65230dd8bb690bab697ff
shravan <bshravankumar0@gmail.com>
2023-10-07 17:35:05 +0530

    First Commit

---
+++ ApiGraphQl/src/test/java/com/resources/APIResources.java
@@ -0,0 +1,21 @@
+package com.resources;
+
+public enum APIResources {
+
+	createDataAPI("/gq/graphql"),
+	getDataAPI("/gq/graphql"),
+	deleteDataAPI("/gq/graphql");
+	
+	private String resource;
+	
+	APIResources(String resource){
+		this.resource=resource;
+	}
+	
+
+	public String getResource() {
+	
+		return resource;
+	}
+	
+}
